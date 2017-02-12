b39aada0808d24848b7a814a956ebccf2bb6cca8
Dmitry Chabanenko <chdn6026@mail.ru>
2017-02-12 22:54:52 +0200

    Initial commit (gradle project, spring deps)

---
+++ src/main/java/com/github/chabanenk0/Main.java
@@ -0,0 +1,11 @@
+package com.github.chabanenk0;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"car_beans.xml"});
+        //Car car = context.getBean("car");
+        System.out.println("Hello, world!");
+    }
+}
